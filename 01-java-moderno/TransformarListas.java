9fa15eb913437d24e1ed46f5ae7a2addcbaa49aa
ytendilloruiz <ytendilloruiz@gmail.com>
2026-03-31 16:59:16 +0200

    feat: uso de streams, filtros y reducciones en Java

---
+++ 01-java-moderno/TransformarListas.java
@@ -0,0 +1,40 @@
+List<String> ciudades = Arrays.asList("madrid", "barcelona", "valencia");
+
+List<String> ciudadesFinales = ciudades.stream()
+.filter (c -> c.length() > 7)
+.map(String::toUpperCase)
+.toList(); // java 16+
+
+//.collect(Collectors.toList()) // Java 8
+
+// Java 5
+
+String encontrada = null;
+for (String c : ciudades) {
+    if (c.startsWith("v")) {
+        encontrada = c;
+        break; 
+    }
+}
+if (encontrada != null) {
+    System.out.println(encontrada);
+}
+
+
+ciudades.stream()
+        .filter(c -> c.startsWith("v"))
+        .findFirst()
+        .ifPresent(System.out::println);
+
+String resultado = encontrada.orElse("No hay ciudades con V");
+// String resultado = encontrada.orElseThrow(() -> new RuntimeException("¡Falta la ciudad!"));
+
+// Ejercicio finale
+
+List<String> numerosSucios = Arrays.asList("10", "20", "cinco", "30", "40");
+
+Integer sumaNumerosTotales = numerosSucios.stream()
+                                            .filter( n -> n.length() < 3)
+                                            .map(Integer::valueOf)
+                                            .filter( n -> n > 25)
+                                            .mapToInt(n -> n).sum();
